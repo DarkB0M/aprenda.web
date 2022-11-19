eb5b12e9317eb0d6f23b9bc1010759d5a8cbbe38
Your Name <you@example.com>
2022-11-18 23:04:31 -0300

    Next-App + Radix

--- pages/Calculadora/index.tsx
+++ pages/Calculadora/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Head from '../../components/Head'
 // import { Container } from './styles';
 // Radix para a barra lateral para mudar o design como Classico,Iphone ETC
+import {} from '@radix-ui/react-navigation-menu'
 const Calculadora: React.FC = () => {
   return(
     <div>
